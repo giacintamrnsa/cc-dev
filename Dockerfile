ce8f23a7d7be84149e971770edf43e550beffdcd
giacintamrnsa <giacintamaurensa@gmail.com>
2022-05-27 13:54:40 +0700

    update db

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM node:16
-RUN apt-get update-y \
-    && apt-get clean
 WORKDIR /usr/src/app
-COPY ./usr/src/appENV port=8080
+COPY package.json /usr/src/app
+RUN npm install
+COPY . /usr/src/app/
+ENV port=8080
 EXPOSE 8080
 CMD ["npm", "start"]
--- database.js
+++ database.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-const {createPool} = require('mysql');
+//const pool = require('mysql/lib/Pool');
+//const mysql = require('mysql2');
+const { createPool } = require('mysql');
 
 const pool = createPool({
     port: process.env.DB_PORT,
@@ -7,7 +9,28 @@ const pool = createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.MYSQL_DB,
-    connectionLimit: 10
+    socketPath: process.env.INSTANCE_CONNECTION_NAME
 });
+
+// if(process.env.NODE_ENV === 'production') {
+//     console.log("Running from cloud. Connecting to DB through GCP socket.");
+//     config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+//   }
+
+// else {
+//     console.log('Running from localhost. Connecting to DB directly.');
+//     config.host = process.env.DB_HOST;
+// }
+
+// let connect = mysql.createConnection(config);
+
+// connect.connect(function(err) {
+//     if (err) {
+//         console.log("Error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("Connected as thread id: " + connect.threadId)
+// })
+
 
 module.exports = pool;
