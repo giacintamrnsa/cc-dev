25a5935f723bed700e1c0834e5cf1eac80c6b3f0
giacintamrnsa <giacintamaurensa@gmail.com>
2022-06-01 17:00:00 +0700

    update content type

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config
 const express = require ('express');
 const app = express();
+const bodyParser = require('body-parser');
 const uRouter = require('./src/router');
 
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/users', uRouter);
 
